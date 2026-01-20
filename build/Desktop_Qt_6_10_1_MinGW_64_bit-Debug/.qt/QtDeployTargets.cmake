e9e7b1e96885524ceed7e76d698164d718238945
William Pepple <williampepple1@gmail.com>
2026-01-20 17:36:00 +0100

    Initial commit

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_clipboard-manager_FILE C:/Users/PC/Documents/GitHub/clipboard-manager/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/clipboard-manager.exe)
+set(__QT_DEPLOY_TARGET_clipboard-manager_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_clipboard-manager_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
